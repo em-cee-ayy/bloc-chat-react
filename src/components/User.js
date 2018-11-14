ca85697cbbc5df3d2f7f185bf8b7b5d03f0e6bd8
Mariah <mariah.c.anderson@gmail.com>
2018-11-14 18:49:15 -0500

    fixed sign in issue

--- src/components/User.js
+++ src/components/User.js
@@ -21,7 +21,7 @@ class User extends Component {
   render() {
     return (
       <section className="user-display">
-        <div className="user-display-name">{ this.props.user ? this.props.user.displayName : ' ' }</div>
+        <div className="user-display-name">{ this.props.user ? this.props.user.displayName : '' }</div>
         <button className="sign-in-out" onClick={ this.props.user ? this.signOut.bind(this) : this.signIn.bind(this) }>
           <span>Sign { this.props.user ? 'out' : 'in' }</span>
         </button>
